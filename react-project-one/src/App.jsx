5a657c4ff1980af5c04c981672cdaeae1a9718e7
Priyanshu Biswas <priyanshu.biswas9041@gmail.com>
2024-12-08 09:52:21 +0530

    addSae

--- react-project-one/src/App.jsx
+++ react-project-one/src/App.jsx
@@ -2,6 +2,7 @@ import UserCard from "./components/UserCard"
 import isagi from "./assets/isagi.jpg"
 import nagi from "./assets/nagi.jpg"
 import rin from "./assets/rin.jpg"
+import sae from "./assets/sae.jpg"
 import './App.css'
 function App() {
   
@@ -13,6 +14,8 @@ function App() {
       <UserCard name="NAGI" description ="lazy" image={nagi} style = {{"border-radius":"10px"}}/>
       
       <UserCard name="ITOSHI RIN" description ="revenge"image={rin} style = {{"border-radius":"10px"}} />
+
+      <UserCard name="ITOSHI SAE" description ="prodigy"image={sae} style = {{"border-radius":"10px"}} />
     </div>
   )
 }
